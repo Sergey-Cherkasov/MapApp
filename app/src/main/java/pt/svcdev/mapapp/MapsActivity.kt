6d3af25155a01ccd1e7bab2e05b5ace242efae9e
Sergio Cherkasov <sergey.cherkasov.developer@gmail.com>
2021-06-22 17:43:21 +0700

    Displays the current location, adds markers on the map

--- app/src/main/java/pt/svcdev/mapapp/MapsActivity.kt
+++ app/src/main/java/pt/svcdev/mapapp/MapsActivity.kt
@@ -1,12 +1,16 @@
 package pt.svcdev.mapapp
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Criteria
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.maps.*
 
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import pt.svcdev.mapapp.databinding.ActivityMapsBinding
@@ -15,6 +19,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+
+    private val onMapLongClickListener = GoogleMap.OnMapLongClickListener {
+        mMap.addMarker(MarkerOptions().position(it))
+    }
+
+    private val onMarkerClickListener = GoogleMap.OnMarkerClickListener {
+        it.remove()
+        return@OnMarkerClickListener true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +39,38 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        requestCurrentLocation()
+    }
+
+    private fun requestCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) return
+        val locationManager: LocationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        val criteria = Criteria()
+        criteria.accuracy = Criteria.ACCURACY_COARSE
+        val provider = locationManager.getBestProvider(criteria, true)
+        provider?.let {
+            locationManager.requestLocationUpdates(it, 10000, 50F,
+                object: LocationListener {
+                    override fun onLocationChanged(location: Location) {
+                        val currentLatLng = LatLng(location.latitude, location.longitude)
+                        mMap.clear()
+                        mMap.addMarker(MarkerOptions()
+                            .position(currentLatLng))
+                        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng))
+                        mMap.setMinZoomPreference(15.0f)
+                    }
+                    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+                    }
+                }
+            )
+        }
     }
 
     /**
@@ -39,10 +84,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.setOnMapLongClickListener(onMapLongClickListener)
+        mMap.setOnMarkerClickListener(onMarkerClickListener)
     }
+
 }
